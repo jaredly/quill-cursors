cdf3563a8d02be4a6724e8e8ada449201e4c8f66
Jared Forsyth <jared@jaredforsyth.com>
2020-02-19 15:41:15 -0700

    gitpkg

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var quill_cursors_1 = __importDefault(require("./quill-cursors/quill-cursors"));
+exports.default = quill_cursors_1.default;
+var cursor_1 = __importDefault(require("./quill-cursors/cursor"));
+exports.Cursor = cursor_1.default;
+require("../assets/quill-cursors.scss");
+//# sourceMappingURL=index.js.map
